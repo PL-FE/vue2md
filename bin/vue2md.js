46f921779b0378ee856b2ff47f71e5adf281c6bb
pengliang <liang.peng@resico.cn>
2023-06-30 17:28:02 +0800

    控制json文件

--- bin/vue2md.js
+++ bin/vue2md.js
@@ -5,6 +5,7 @@ const outputMd = require("../lib/outputMd");
 const { createFile } = require("../lib/utils");
 // 从命令行参数中获取文件路径
 const filePath = process.argv[2];
+const options = process.argv;
 
 if (!filePath) {
   console.error("Please provide a valid file path");
@@ -12,7 +13,9 @@ if (!filePath) {
   Vuedoc
     .parse({ filename: filePath })
     .then((component) => {
-      createFile(component, filePath, '.json');
+      if (options.find(a => a === '-json')) {
+        createFile(component, filePath, '.json');
+      }
       outputMd(component, filePath);
     })
     .catch((err) => console.error(err));
