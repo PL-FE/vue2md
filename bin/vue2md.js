1a8a4759556b43b3f64f0884e96be040ad531575
pengliang <liang.peng@resico.cn>
2023-06-30 15:27:25 +0800

    md文件

--- bin/vue2md.js
+++ bin/vue2md.js
@@ -1,28 +1,19 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
-
 const Vuedoc = require("@vuedoc/parser");
-
+const outputMd = require("../lib/outputMd");
+const { createFile } = require("../lib/utils");
 // 从命令行参数中获取文件路径
 const filePath = process.argv[2];
 
 if (!filePath) {
   console.error("Please provide a valid file path");
 } else {
-  const options = {
-    filename: filePath,
-  };
-
-  Vuedoc.parse(options)
+  Vuedoc
+    .parse({ filename: filePath })
     .then((component) => {
-      const markdownFilePath =
-        path.join(
-          path.dirname(filePath),
-          path.basename(filePath, path.extname(filePath))
-        ) + ".json";
-      fs.writeFileSync(markdownFilePath, JSON.stringify(component), "utf-8");
+      createFile(component, filePath, '.json');
+      outputMd(component, filePath);
     })
     .catch((err) => console.error(err));
 }
--- test/test.vue
+++ test/test.vue
@@ -1,75 +1,274 @@
 <template>
-  <div
-    class="el-select-dropdown el-popper"
-    :class="[{ 'is-multiple': $parent.multiple }, popperClass]"
-    :style="{ minWidth: minWidth }"
-  >
+  <label>
+    <input :disabled="disabled"
+      type="text"
+      v-model="checkbox" />
     <slot></slot>
-  </div>
+    <!-- Use this slot to set the checkbox label -->
+    <slot name="label"
+      :initialValue="initialValue"></slot>
+  </label>
 </template>
 
-<script type="text/babel">
-import Popper from "element-ui/src/utils/vue-popper";
+<script>
+const dynamic2 = 'dynamicMode'
+const zero = 0
+const nullValue = null
+const undefinedValue = undefined
+const emptyString = ''
+const boolFalse = false
 
+/**
+ * A simple checkbox component
+ *
+ * ```html
+ * <checkbox v-model="value"/>
+ * ```
+ *
+ * @author Sébastien
+ * @license MIT
+ */
 export default {
-  name: "ElSelectDropdown",
-
-  componentName: "ElSelectDropdown",
-
-  mixins: [Popper],
-
+  name: 'checkbox',
   props: {
-    placement: {
-      default: "bottom-start",
+    /**
+     * The checkbox model
+     */
+    model: {
+      type: Array,
+      required: true
     },
 
-    boundariesPadding: {
-      default: 0,
-    },
-
-    popperOptions: {
-      default() {
+    testPorps: {
+      default () {
         return {
-          gpuAcceleration: true,
-        };
-      },
+          a: 1
+        }
+      }
     },
 
-    visibleArrow: {
-      default: true,
-    },
+    /**
+     * Initial checkbox state
+     */
+    disabled: Boolean,
 
-    appendToBody: {
+    /**
+     * Initial checkbox value
+     */
+    enabled: {
       type: Boolean,
-      default: true,
+      default: true
     },
+
+    /**
+     * The checkbox label
+     */
+    label: {
+      type: String,
+      default: 'Unamed checkbox'
+    },
+
+    /**
+     * The checkbox custom type object
+     * @type CustomType
+     */
+    object: {
+      type: Object,
+      default: null
+    },
+
+    boolFalse: {
+      type: Boolean,
+      default: false
+    },
+
+    int: {
+      type: Number,
+      default: 100_000_000
+    },
+
+    /**
+     * @default {}
+     */
+    propWithDefaultAsKeywordButWithoutDefault: {
+      type: Object
+    },
+
+    /**
+     * @default {}
+     */
+    propWithDefaultAsKeyword: {
+      type: Object,
+      default: () => ({})
+    },
+
+    /**
+     * @default
+     */
+    propWithEmptyDefaultAsKeyword: {
+      type: Object,
+      default: () => ({})
+    },
+
+    /**
+     * @default 0
+     */
+    numberPropWithDefaultAsKeyword: {
+      type: Number,
+      default: zero
+    },
+
+    /**
+     * @default ''
+     */
+    stringPropWithDefaultAsKeyword: {
+      type: String,
+      default: emptyString
+    },
+
+    /**
+     * @default false
+     */
+    booleanPropWithDefaultAsKeyword: {
+      type: Boolean,
+      default: boolFalse
+    },
+
+    /**
+     * @default empty array
+     */
+    arrayPropWithDefaultAsKeyword: {
+      type: Array,
+      default: () => ([])
+    },
+
+    /**
+     * The input validation function
+     * @kind function
+     * @param {any} value - User input value to validate
+     * @returns {boolean} - `true` if validation succeeds; `false` otherwise.
+     */
+    validator: {
+      type: Function,
+      default: (value) => !Number.isNaN(value)
+    },
+
+    /**
+     * @default null
+     */
+    propWithNullAsDefaultKeyword: {
+      type: Object,
+      default: nullValue
+    },
+
+    /**
+     * @default undefined
+     */
+    propWithUndefinedAsDefaultKeyword: {
+      type: Object,
+      default: undefinedValue
+    }
   },
 
-  data() {
+  created () {
+    /**
+     * Emitted when the component has been loaded
+     */
+    this.$emit('loaded')
+  },
+
+  data () {
     return {
-      minWidth: "",
-    };
+      /**
+       * The initial component value.
+       * Used to detect changes and restore the initial value.
+       */
+      initialValue: '',
+      currentValue: '',
+      data: {
+        a: 1,
+        b: {
+          c: 3
+        }
+      }
+    }
   },
 
   computed: {
-    popperClass() {
-      return this.$parent.popperClass;
+    /**
+     * The component identifier.
+     * Generated using the `initialValue` data.
+     */
+    id () {
+      return `checkbox-${this.initialValue}`
     },
+
+    changed () {
+      return this.currentValue !== this.initialValue
+    },
+
+    withNoDependencies () {
+      return 'yeah'
+    }
   },
 
-  watch: {
-    "$parent.inputWidth": function () {
-      this.minWidth = `${this.$parent.$el.getBoundingClientRect().width}px`;
+  methods: {
+    /**
+     * Check if the input is checked
+     */
+    check () {
+      console.log('check')
     },
-  },
 
-  mounted() {
-    this.referenceElm = this.$parent.$refs.reference.$el;
-    this.$parent.popperElm = this.popperElm = this.$el;
-    this.$on("updatePopper", () => {
-      if (this.$parent.visible) this.updatePopper();
-    });
-    this.$on("destroyPopper", this.destroyPopper);
-  },
+    prop: () => {
+      console.log('prop')
+    },
+
+    // Make component dynamic
+    ['dynamic']: () => {
+      console.log('dynamic')
+    },
+
+    // Enter to dynamic mode
+    [dynamic2]: () => {
+      console.log(dynamic2)
+    },
+
+    /*
+     * This should be ignored
+     * @private
+     */
+    ignored () { },
+
+    // Another ignored method
+    // @private
+    anotherIgnored () { },
+
+    // Enable the checkbox
+    enable (value) {
+      const x = true
+      /**
+       * Emitted the event `enabled` when loaded
+       * Multilign
+       */
+      this.$emit('enabled', x)
+      this.$emit('updateEvent', { flag: value })
+    },
+
+    // 测试注释
+    testMethods (name, age) {
+      return name + 1
+    },
+
+    /**
+     * 标准注释
+     */
+    testMethods2 (name, age) {
+      return name + 1
+    }
+  }
 };
 </script>
+
+<style lang="css" scoped>
+</style>
